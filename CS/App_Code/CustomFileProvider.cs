6271b069487378f88eaed2b4caccda0f0ac8c3a2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/App_Code/CustomFileProvider.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+public class CustomFileProvider : PhysicalFileSystemProvider {
+	public CustomFileProvider(string rootFolder)
+		: base(rootFolder) { }
+	public override IEnumerable<FileManagerFolder> GetFolders(FileManagerFolder parentFolder) {
+		List<FileManagerFolder> folders = new List<FileManagerFolder>(base.GetFolders(parentFolder));
+		return folders.OrderByDescending(a => a.Name);
+	}
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to show elements in ASPxFileManager in a custom order
+
+
+<p>This example demonstrates how to make <a href="https://documentation.devexpress.com/#AspNet/clsDevExpressWebASPxFileManagertopic"><strong>ASPxFileManager</strong></a> show folders in the descending order. This behavior was implemented by using the <strong>CustomFileProvider</strong> class.</p>
+
+<br/>
+
+
